45d5cd8fbae0d62cd259e7c4c0bd089e559ec20f
AntonioTSouza <tonitsouza2@gmail.com>
2025-11-26 11:20:45 -0300

    back-end

--- core/conexao_mysql.php
+++ core/conexao_mysql.php
@@ -2,7 +2,7 @@
     function conecta(): mysqli
     {
         $servidor = 'localhost';
-        $banco = 'blog';
+        $banco = 'ifsports';
         $port = 3306;
         $usuario = 'root';
         $senha = '';
